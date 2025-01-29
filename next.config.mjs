231d31ff7221f15ecae672d760bbc3d6e152e52b
SmallDevStudio <smalldevstudio@gmail.com>
2025-01-29 22:44:55 +0700

    cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["localhost"],
+  },
+  cacheHandler: "./cache-handler.js",
+  cacheMaxMemorySize: 1048576,
+  experimental: {
+    cache: true,
+  },
 };
 
 export default nextConfig;
